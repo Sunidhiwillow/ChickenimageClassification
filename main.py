87559054df502163fae9d56b1d28fa51ee3b572a
Sunidhiwillow <sunidhiprakash22@gmail.com>
2023-10-25 10:13:38 +0530

    basemodelprepared

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from CICD import logger
 #logger.info("Welcome to my custom log") 
 from CICD.pipeline.stage01_dataingestion import DataIngestionTrainingPipeline
+from CICD.pipeline.stage02_basemodel import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -12,5 +13,14 @@ except Exception as e:
     logger.exception(e)
     raise e
 
-
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
